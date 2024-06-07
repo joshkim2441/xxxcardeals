aa8294b6ea92e03fc81073b3ae78105f1d75f6b8
joshkim2441 <joshkim2441@gmail.com>
2024-06-06 22:53:21 -0700

    Script to pack and deploy static

---
+++ pack_deploy_static.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Fabric script that creates and
+distributes an archive to the web servers
+
+execute: fab -f pack_deploy_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+"""
+
+from fabric.api import *
+from datetime import datetime
+import os
+env.user = 'ubuntu'
+env.key_filename = "~/id_rsa"
+env.hosts = ['34.232.53.167', '54.89.195.92']
+
+
+def do_pack():
+    """ Generates a .tgz archive from contents
+    of the web_static folder
+    """
+    tm = datetime.now()
+    tm_ft = tm.strftime("%Y%m%d%H%M%S")
+    if not os.path.isdir("versions"):
+        local("mkdir versions")
+    file_path = "versions/static_{}.tgz".format(tm_ft)
+    result = local("tar -cvzf {} static".format(file_path))
+    if result.failed:
+        return None
+    archive_size = os.stat(file_path).st_size
+    print("static packed: {} -> {} Bytes".format(file_path, archive_size))
+    return file_path
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/static'.format(path, no_ext))
+        run('rm -rf /data/static/current')
+        run('ln -s {}{}/ /data/static/current'.format(path, no_ext))
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """
+        deploy function that creates/distributes an archive
+    """
+    archive_path = do_pack()
+    if archive_path is None:
+        print("do_pack failed")
+        return False
+    return do_deploy(archive_path)
+
